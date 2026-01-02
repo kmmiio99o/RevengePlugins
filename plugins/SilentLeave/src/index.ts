6da7212a8ad8a952b29515d7745db95288d01190
coder <243643262+il1ll@users.noreply.github.com>
2026-01-02 21:06:13 +0300

    Update index.ts

--- plugins/SilentLeave/src/index.ts
+++ plugins/SilentLeave/src/index.ts
@@ -3,7 +3,7 @@ import { findByProps } from "@vendetta/metro";
 import { showToast } from "@vendetta/ui/toasts";
 
 const ChannelStore = findByProps("getChannel");
-const getToken = findByProps("getToken").getToken;
+const APIUtils = findByProps("getAPIBaseURL", "del");
 
 const loadCommands = () => {
   registerCommand({
@@ -15,28 +15,16 @@ const loadCommands = () => {
       const channel = ChannelStore.getChannel(channelId);
       return !!channel && channel.type === 3;
     },
-    execute: (_, ctx) => {
-      const channelId = ctx.channel.id;
-      const token = getToken();
-
-      if (!token) {
-        showToast("Failed to get token.");
-        return;
+    execute: async (_, ctx) => {
+      try {
+        await APIUtils.del({
+          url: `/channels/${ctx.channel.id}`,
+          query: "silent=true"
+        });
+        showToast("Left Group DM successfully.");
+      } catch {
+        showToast("Failed to leave Group DM.");
       }
-
-      fetch(`https://discord.com/api/v9/channels/${channelId}?silent=true`, {
-        method: "DELETE",
-        headers: {
-          "Authorization": token,
-          "User-Agent": "Discord-Android/305012;RNA",
-          "Accept-Encoding": "gzip"
-        }
-      })
-      .then(res => {
-        if (res.ok) showToast("Left Group DM successfully.");
-        else showToast(`Failed: ${res.status}`);
-      })
-      .catch(e => showToast(e.message));
     }
   });
 };
